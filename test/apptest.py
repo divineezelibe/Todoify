62ac6e6e84ffd91bf83aa7d851787a0d558eea40
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-12-14 23:43:27 +0100

    Created a test, to check out all my dependencies are working

---
+++ test/apptest.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import sqlite3
+
+# Your Tkinter and SQLite code here
+
+# For example:
+root = tk.Tk()
+root.title("My Tkinter App")
+
+# Your Tkinter widgets and logic
+
+root.mainloop()
